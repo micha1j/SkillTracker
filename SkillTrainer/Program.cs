694094d0603652c94b4e929d3d3973f97ce9b9e8
Michal Chrapowicki <michal333878@gmail.com>
2025-07-15 01:16:07 +0200

    Initial commit with .gitignore

---
+++ SkillTrainer/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+public class Skill
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<StudySession> StudySessions { get; set; } = new();
+}
+public class StudySession
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int DurationMinutes { get; set; }
+    public string Notes { get; set; }
+
+    public int SkillId { get; set; }
+    public Skill Skill { get; set; }
+}
