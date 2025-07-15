5e6832f3b22b0187455e4834e904824d79be8ffb
Michal Chrapowicki <michal333878@gmail.com>
2025-07-15 22:16:59 +0200

    Dodanie funkcji

---
+++ SkillTrainer/Migrations/20250715191020_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkillTrainer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudySessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DurationMinutes = table.Column<int>(type: "INTEGER", nullable: false),
+                    Notes = table.Column<string>(type: "TEXT", nullable: false),
+                    SkillId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudySessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_StudySessions_Skills_SkillId",
+                        column: x => x.SkillId,
+                        principalTable: "Skills",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StudySessions_SkillId",
+                table: "StudySessions",
+                column: "SkillId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudySessions");
+
+            migrationBuilder.DropTable(
+                name: "Skills");
+        }
+    }
+}
--- SkillTrainer/Program.cs
+++ SkillTrainer/Program.cs
@@ -1,18 +1,65 @@
-﻿Console.WriteLine("Hello, World!");
-public class Skill
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
+﻿using Microsoft.EntityFrameworkCore;
 
-    public List<StudySession> StudySessions { get; set; } = new();
-}
-public class StudySession
+namespace SkillTrainer
 {
-    public int Id { get; set; }
-    public DateTime Date { get; set; }
-    public int DurationMinutes { get; set; }
-    public string Notes { get; set; }
+    class Program
+    {
+        static void UtwórzSkill(AppDbContext context, string name)
+        {
+            bool takasamanazwa = context.Skills.Any(s => s.Name == name);
+            if (takasamanazwa == true) { Console.WriteLine("Ten skill już istnieje"); return; }
+            context.Skills.Add(new Skill { Name = name });
+            context.SaveChanges();
+        }
+        static void UtwórzStudySession(AppDbContext context, string skillname, DateTime data, int durationMinutes, string notes)
+        {
+            bool takasamanazwa = context.Skills.Any(s => s.Name == skillname);
+            if (takasamanazwa == false)
+            {
+                UtwórzSkill(context, skillname);
+            }
+            var skill = context.Skills.SingleOrDefault(s => s.Name == skillname);
+            if (skill == null)
+            {
+                throw new InvalidOperationException("Skill nie został poprawnie utworzony lub pobrany z bazy.");
+            }
+            var session = new StudySession { Date = data, DurationMinutes = durationMinutes, Notes = notes, Skill = skill};
+            context.StudySessions.Add(session);
+            context.SaveChanges();
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            using var context = new AppDbContext();
+        }
+    }
 
-    public int SkillId { get; set; }
-    public Skill Skill { get; set; }
+    public class Skill
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<StudySession> StudySessions { get; set; } = new();
+    }
+
+    public class StudySession
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int DurationMinutes { get; set; }
+        public string Notes { get; set; }
+
+        public int SkillId { get; set; }
+        public Skill Skill { get; set; }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<StudySession> StudySessions { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=skilltrack.db");
+        }
+    }
 }
